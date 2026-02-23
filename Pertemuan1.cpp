9097bf8227a9c3b1714feaef6b3c524e7f8fe951
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-02-23 21:49:07 +0700

    variabel2

---
+++ Pertemuan1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//variabel global 
+int sks;
+
+int main(){
+    //variabel local
+    int umur;
+    umur = 20;
+
+    cout << "hallo almaidah" << endl;
+    cout << 10 << endl;
+    cout << umur;
+}
